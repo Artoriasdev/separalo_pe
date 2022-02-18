95e8d89fe2238e56ede5ef14274c99f782c3bfcf
Hirikari1 <piero.bryan@hotmail.com>
2022-02-17 19:41:22 -0500

    Imagen añadida al carousel

--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -1,6 +1,7 @@
 import { Paper } from "@mui/material";
 import React from "react";
 import Carousel from "react-material-ui-carousel";
+import Image from "../assets/images/separalo_image.jpg";
 
 function Project(props) {
   return (
@@ -23,7 +24,7 @@ function Project(props) {
 const items = [
   {
     id: "30%",
-    link: "https://scontent.flim29-1.fna.fbcdn.net/v/t39.30808-6/256250205_127605489691435_2216501464502058653_n.jpg?_nc_cat=102&ccb=1-5&_nc_sid=e3f864&_nc_eui2=AeGUoUTejDJEMS3yJcQIyffOtEqQvFXJami0SpC8VclqaH0LRE_XKsoODPFJelqIFrKAJL7_6_EUdLQQEULdtw3_&_nc_ohc=lR1VWaELzlwAX_ctvn-&_nc_ht=scontent.flim29-1.fna&oh=00_AT8NWcz9LMYWX5mnZggfz0rK8NBLm3IX94F3cdF3Gq_auQ&oe=620C0A99",
+    link: Image,
     category: "4",
     service: "3",
   },
