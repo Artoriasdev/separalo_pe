757278d3bb026c82d9095e9d40d10a0a113cfafc
Hirikari1 <piero.bryan@hotmail.com>
2022-02-17 19:44:06 -0500

    Regex de correo modificado

--- src/utils/regexp.js
+++ src/utils/regexp.js
@@ -1,5 +1,5 @@
 export const EMAIL_REGEXP =
-  /^[a-zA-Z0-9][a-zA-Z0-9.-]*[a-zA-Z0-9]@[a-zA-Z0-9][a-zA-Z0-9.-]*[a-zA-Z0-9]\.[a-zA-Z]{2,63}$/;
+  /^(([^<>()\\.,;:\s@"]+(\.[^<>()\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 export const NUMBER_REGEXP = /[0-9]/;
 export const NUMBER_START_REGEXP = /^[9]/;
 export const PASSWORD_REGEXP = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[\w\W]{8,}$/;
